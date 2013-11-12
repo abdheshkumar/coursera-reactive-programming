0e01e757a293a7685c6933c924a4421f3f7baf19
Nitay Joffe <nitayj@fb.com>
2013-11-12 02:59:12 -0500

    start impl epidemic

--- a2/src/main/scala/simulations/EpidemySimulator.scala
+++ a2/src/main/scala/simulations/EpidemySimulator.scala
@@ -1,35 +1,142 @@
 package simulations
 
 import math.random
+import scala.collection.mutable
 
 class EpidemySimulator extends Simulator {
 
-  def randomBelow(i: Int) = (random * i).toInt
+  def randomBelow(i: Int): Int = (random * i).toInt
+
+  def biasedCoin(trueProb: Double): Boolean = trueProb <= random
 
   protected[simulations] object SimConfig {
     val population: Int = 300
     val roomRows: Int = 8
     val roomColumns: Int = 8
 
-    // to complete: additional parameters of simulation
+    val becomeSickDay = 6
+    val maybeDieDay = 14 - becomeSickDay
+    val becomeImmuneDay = 16 - maybeDieDay
+    val becomeHealthyDay = 18 - becomeImmuneDay
+
+    val prevalence = 0.01
+    val transmissibilityRate = 0.4
+    val deathRate = 0.25
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val persons: List[Person] = (0 to population).map(id => new Person(id)).toList
+
+  case class Room(row: Int, col: Int) {
+    private def isAdjacent(x1: Int, x2: Int, max: Int) = {
+      val d = (x1 - x2).abs
+      d == 1 || d == max - 1
+    }
+
+    private def isRowAdjacent(room: Room) = isAdjacent(row, room.row, roomRows)
+    private def isColAdjacent(room: Room) = isAdjacent(col, room.col, roomColumns)
+
+    def up = Room((row - 1 + roomRows) % roomRows, col)
+    def down = Room((row + 1) % roomRows, col)
+    def left = Room(row, (col - 1 + roomColumns) % roomColumns)
+    def right = Room(row, (col + 1) % roomColumns)
+
+    def isAdjacent(room: Room): Boolean =
+      (isRowAdjacent(room) && col == room.col) || (isColAdjacent(room) && row == room.row)
+
+    def adjacentSet: mutable.HashSet[Room] = mutable.HashSet(up, down, left, right)
+
+    def hasInfectiousPeople = persons.exists(p => this == p.room && p.isInfectous)
+  }
 
   class Person (val id: Int) {
-    var infected = false
-    var sick = false
-    var immune = false
-    var dead = false
+    var infected: Boolean = id < (population * prevalence).toInt
+    var sick: Boolean = false
+    var immune: Boolean = false
+    var dead: Boolean = false
 
-    // demonstrates random number generation
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    def isInfectous: Boolean = infected || sick || immune || dead
+    def isHealthy: Boolean = !isInfectous
+    def canBeInfected: Boolean = isHealthy
+    def isVisiblyInfected: Boolean = sick || dead
+
+    def neighbors = persons.withFilter(p => isNeighbor(p))
+
+    def visiblyInfectedNeighbors = neighbors.withFilter(p => p.isVisiblyInfected)
+
+    def room: Room = Room(row, col)
+
+    def becomeHealthy() {
+      assert(infected)
+      assert(sick)
+      assert(immune)
+      assert(!dead)
+      infected = false
+      sick = false
+      immune = false
+    }
+
+    def becomeImmune() {
+      assert(infected)
+      assert(sick)
+      assert(!immune)
+      assert(!dead)
+      immune = true
+      afterDelay(becomeHealthyDay)(becomeHealthy)
+    }
+
+    def maybeDie() {
+      assert(infected)
+      assert(sick)
+      assert(!immune)
+      assert(!dead)
+      if (biasedCoin(deathRate)) {
+        dead = true
+      } else {
+        afterDelay(becomeImmuneDay)(becomeImmune)
+      }
+    }
+
+    def becomeSick() {
+      assert(infected)
+      assert(!sick)
+      assert(!immune)
+      assert(!dead)
+      sick = true
+      afterDelay(maybeDieDay)(maybeDie)
+    }
+
+    def moveToRoom(room: Room) {
+      row = room.row
+      col = room.col
+      if (canBeInfected && room.hasInfectiousPeople && biasedCoin(transmissibilityRate)) {
+        infected = true
+        afterDelay(becomeSickDay)(becomeSick)
+      }
+    }
+
+    def isNeighbor(p: Person): Boolean = room.isAdjacent(p.room)
+
+    def validAdjacentRooms = {
+      val validRooms = room.adjacentSet
+      visiblyInfectedNeighbors.foreach(p => validRooms.remove(p.room))
+      validRooms.toList
+    }
+
+    def tryMoveToNeighboringRoom() {
+      if (!dead) {
+        val rooms = validAdjacentRooms
+        if (!rooms.isEmpty) {
+          val x = randomBelow(rooms.size)
+          moveToRoom(rooms(x))
+        }
+      }
+    }
+
+    afterDelay(randomBelow(5))(tryMoveToNeighboringRoom())
   }
 }
