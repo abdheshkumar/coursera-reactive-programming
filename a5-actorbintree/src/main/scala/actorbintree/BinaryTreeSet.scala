1c67eec0f9f357c8b14c3ee5ace078086cb18dca
Nitay Joffe <nitayj@fb.com>
2013-12-06 00:45:56 -0500

    start impl a5

--- a5-actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ a5-actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -50,7 +50,7 @@ object BinaryTreeSet {
 }
 
 
-class BinaryTreeSet extends Actor {
+class BinaryTreeSet extends Actor with ActorLogging {
   import BinaryTreeSet._
   import BinaryTreeNode._
 
@@ -66,7 +66,18 @@ class BinaryTreeSet extends Actor {
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case Insert(requester, id, elem) => {
+      root.forward(Insert(requester, id, elem))
+    }
+    case Contains(requester, id, elem) => {
+      root.forward(Contains(requester, id, elem))
+    }
+    case Remove(requester, id, elem) => {
+      root.forward(Remove(requester, id, elem))
+    }
+    case GC => ???
+  }
 
   // optional
   /** Handles messages while garbage collection is performed.
@@ -89,7 +100,7 @@ object BinaryTreeNode {
   def props(elem: Int, initiallyRemoved: Boolean) = Props(classOf[BinaryTreeNode],  elem, initiallyRemoved)
 }
 
-class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
+class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor with ActorLogging {
   import BinaryTreeNode._
   import BinaryTreeSet._
 
@@ -101,12 +112,73 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
 
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case Insert(requester, id, elem) => {
+      propagateOperation(Insert(requester, id, elem), Inserter)
+    }
+    case Contains(requester, id, elem) => {
+      propagateOperation(Contains(requester, id, elem), ContainsCheck)
+    }
+    case Remove(requester, id, elem) => {
+      propagateOperation(Remove(requester, id, elem), Remover)
+    }
+  }
+
+  trait Actions {
+    def equalsNode(requester: ActorRef, id: Int): Unit
+    def newNode(position: Position, operation: Operation): Unit
+  }
+
+  object Inserter extends Actions {
+    def equalsNode(requester: ActorRef, id: Int): Unit = {
+      removed = false
+      requester ! OperationFinished(id)
+    }
+    def newNode(position: Position, operation: Operation): Unit = {
+      val newActor = context.actorOf(BinaryTreeNode.props(operation.elem, initiallyRemoved = false),
+        name = "node-" + operation.elem)
+//      log.info("create new " + position + " node for " + operation.elem)
+      subtrees += position -> newActor
+      operation.requester ! OperationFinished(operation.id)
+    }
+  }
+
+  object ContainsCheck extends Actions {
+    def equalsNode(requester: ActorRef, id: Int): Unit = {
+      requester ! ContainsResult(id, !removed)
+    }
+    def newNode(position: Position, operation: Operation): Unit = {
+//      log.info("contains " + operation + " => false")
+      operation.requester ! ContainsResult(operation.id, result = false)
+    }
+  }
+
+  object Remover extends Actions {
+    def equalsNode(requester: ActorRef, id: Int): Unit = {
+      removed = true
+      requester ! OperationFinished(id)
+    }
+    def newNode(position: Position, operation: Operation): Unit = {
+//      log.info("remove non existing node")
+      operation.requester ! OperationFinished(operation.id)
+    }
+  }
+
+  def propagateOperation(operation: Operation, actions: Actions) {
+    if (operation.elem == this.elem) {
+      actions.equalsNode(operation.requester, operation.id)
+    } else {
+      val position = if (operation.elem < this.elem) Left else Right
+      subtrees.get(position) match {
+        case Some(actor) => actor.forward(operation)
+        case None => actions.newNode(position, operation)
+      }
+    }
+  }
 
   // optional
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
     * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
     */
   def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
-
 }
